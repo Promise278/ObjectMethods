9e59120faa17419220e6868e6c675374bc904f93
Promise278 <promiseobi2007@gmail.com>
2025-04-15 12:05:33 +0100

    added changes

--- task3.js
+++ task3.js
@@ -1,20 +1,19 @@
-
 // 3. Create a settings object (e.g., theme: "dark", notifications: true). Freeze it and try changing a value. Use Object.isFrozen to confirm if it's immutable.
 
 let settings = {
     theme: "dark",
-    notification: "true",
+    notification: true,
 }
 
 Object.freeze(settings)
 
 
-if (Object.isFrozen(settings)) {
-    console.log("settings is in state of lock down cant be change")
-} else {
-    settings.theme = "white"
-}
+// if (Object.isFrozen(settings)) {
+//     console.log("settings is in state of lock down cant be change")
+// } else {
+//     settings.theme = "white"
+// }
 
 settings.theme = "white"
 
-console.log(settings)
+console.log(Object.isFrozen(settings))
